a58f3fefa2035fd742e48e6db185e7b861cd61af
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2025-08-28 13:21:39 -0300

    Reset btn & naming

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,25 @@ function App() {
                 <Row rowProperties=" d-flex justify-content-center h-100">
                     <Col colProperties="-6 d-flex justify-content-center align-items-center">
                         <CounterBody title="Counter App">
-                            <div id="counterDisplay" className="counter-display d-inline-block rounded-3 text-bg-secondary px-5 py-4 my-3">
-                                <span className="display-2 fw-bold">5</span>
+                            <div id="counterCont" className="counter-cont d-inline-block rounded-3 text-bg-secondary px-5 py-4 my-5">
+                                <span id="counterDisplay" className="counter-display display-2 fw-bold">
+                                    5
+                                </span>
                             </div>
 
-                            <div id="counterControls" className="counter-controls d-flex justify-content-center align-items-center">
-                                <CounterBtn btnBg="danger">
-                                    <i className="fa-solid fa-circle-minus"></i>
-                                </CounterBtn>
-                                <StepInput type="number" />
-                                <CounterBtn btnBg="success">
-                                    <i className="fa-solid fa-circle-plus"></i>
-                                </CounterBtn>
+                            <div id="counterControls" className="counter-controls">
+                                <div className="d-flex justify-content-center align-items-center">
+                                    <CounterBtn btnBg="danger">
+                                        <i className="fa-solid fa-circle-minus"></i>
+                                    </CounterBtn>
+                                    <StepInput type="number" />
+                                    <CounterBtn btnBg="success">
+                                        <i className="fa-solid fa-circle-plus"></i>
+                                    </CounterBtn>
+                                </div>
+                                <div className="d-flex justify-content-center align-items-center">
+                                    <button className="reset-btn rounded-4 fw-bold btn btn-warning px-5 py-2 my-3">Reset</button>
+                                </div>
                             </div>
                         </CounterBody>
                     </Col>
