d1fef836bcb14a87d7c94492bb904ea1e068c475
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2025-08-28 13:06:28 -0300

    Basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import "./App.css";
+import Container from "./components/Container/Container";
+import Row from "./components/Row";
+import Col from "./components/Col";
+import CounterBody from "./components/CounterBody";
+import CounterBtn from "./components/CounterBtn/CounterBtn";
+import StepInput from "./components/StepInput";
 
 function App() {
     return (
         <>
-            <p className="text-primary">Hello, world!</p>
+            <Container>
+                <Row rowProperties=" d-flex justify-content-center h-100">
+                    <Col colProperties="-6 d-flex justify-content-center align-items-center">
+                        <CounterBody title="Counter App">
+                            <div id="counterDisplay" className="counter-display d-inline-block rounded-3 text-bg-secondary px-5 py-4 my-3">
+                                <span className="display-2 fw-bold">5</span>
+                            </div>
+
+                            <div id="counterControls" className="counter-controls d-flex justify-content-center align-items-center">
+                                <CounterBtn btnBg="danger">
+                                    <i className="fa-solid fa-circle-minus"></i>
+                                </CounterBtn>
+                                <StepInput type="number" />
+                                <CounterBtn btnBg="success">
+                                    <i className="fa-solid fa-circle-plus"></i>
+                                </CounterBtn>
+                            </div>
+                        </CounterBody>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
