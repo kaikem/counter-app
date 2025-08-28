9f493e369d5bc2140ac6393859a9f4004191d28d
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2025-08-28 10:38:35 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <p className="text-primary">Hello, world!</p>
+        </>
+    );
+}
+
+export default App;
